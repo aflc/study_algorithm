2c731993d484b576b7673902a2af93f9cecdddd0
Hiroyuki Tanaka <aflc0x@gmail.com>
2014-01-12 15:41:13 +0900

    simplify insert_sort for Ruby

--- ruby/insert_sort.rb
+++ ruby/insert_sort.rb
@@ -3,14 +3,11 @@
 def insert_sort! l
   (1...l.length).each do |i|
     i.downto(1).each do |j|
-      if l[j] < l[j - 1]
-        l[j - 1], l[j] = l[j], l[j - 1]
-      else
-        break
-      end
+      break if l[j] >= l[j - 1]
+      l[j - 1], l[j] = l[j], l[j - 1]
     end
   end
   l
 end
 
-p insert_sort!((1..10).map{ rand(100) })
+p insert_sort!((1..20).map{ rand(100) })
